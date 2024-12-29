4b6f21d3829f6dd6d4f4da212d63830cb5f30965
Muhammad Amien <muhammadamien23@gmail.com>
2024-12-30 01:23:03 +0700

    * feat(main.py): added comparison operations

---
+++ 07_Operasi_komparasi/main.py
@@ -0,0 +1,33 @@
+
+# Contoh Operasi Komparasi di Python
+
+# 1. Sama dengan (==)
+nilai1 = 10
+nilai2 = 20
+hasil_sama_dengan = nilai1 == nilai2
+
+print("Nilai 1:", nilai1)
+print("Nilai 2:", nilai2)
+
+print("Apakah nilai1 sama dengan nilai2?", hasil_sama_dengan)  # Output: False
+
+# 2. Tidak sama dengan (!=)
+hasil_tidak_sama_dengan = nilai1 != nilai2
+print("Apakah nilai1 tidak sama dengan nilai2?", hasil_tidak_sama_dengan)  # Output: True
+
+# 3. Lebih besar dari (>)
+hasil_lebih_besar = nilai1 > nilai2
+print("Apakah nilai1 lebih besar dari nilai2?", hasil_lebih_besar)  # Output: False
+
+# 4. Lebih kecil dari (<)
+hasil_lebih_kecil = nilai1 < nilai2
+print("Apakah nilai1 lebih kecil dari nilai2?", hasil_lebih_kecil)  # Output: True
+
+# 5. Lebih besar atau sama dengan (>=)
+hasil_lebih_besar_sama_dengan = nilai1 >= nilai2
+print("Apakah nilai1 lebih besar atau sama dengan nilai2?", hasil_lebih_besar_sama_dengan)  # Output: False
+
+# 6. Lebih kecil atau sama dengan (<=)
+hasil_lebih_kecil_sama_dengan = nilai1 <= nilai2
+print("Apakah nilai1 lebih kecil atau sama dengan nilai2?", hasil_lebih_kecil_sama_dengan)  # Output: True
+
