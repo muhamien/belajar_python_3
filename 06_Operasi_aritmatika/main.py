91f1c6475939429165c927caa702e872196235d2
Muhammad Amien <muhammadamien23@gmail.com>
2024-12-30 01:19:56 +0700

    * feat(main.py): added arithmetic operations

---
+++ 06_Operasi_aritmatika/main.py
@@ -0,0 +1,33 @@
+
+# Contoh Operasi Aritmatika di Python
+
+# 1. Penjumlahan
+angka1 = 10
+angka2 = 5
+hasil_penjumlahan = angka1 + angka2
+print("Hasil Penjumlahan:", hasil_penjumlahan)  # Output: 15
+
+# 2. Pengurangan
+hasil_pengurangan = angka1 - angka2
+print("Hasil Pengurangan:", hasil_pengurangan)  # Output: 5
+
+# 3. Perkalian
+hasil_perkalian = angka1 * angka2
+print("Hasil Perkalian:", hasil_perkalian)  # Output: 50
+
+# 4. Pembagian
+hasil_pembagian = angka1 / angka2
+print("Hasil Pembagian:", hasil_pembagian)  # Output: 2.0
+
+# 5. Pembagian Bulat
+hasil_pembagian_bulat = angka1 // angka2
+print("Hasil Pembagian Bulat:", hasil_pembagian_bulat)  # Output: 2
+
+# 6. Modulus
+hasil_modulus = angka1 % angka2
+print("Hasil Modulus:", hasil_modulus)  # Output: 0
+
+# 7. Pangkat
+hasil_pangkat = angka1 ** angka2
+print("Hasil Pangkat:", hasil_pangkat)  # Output: 100000
+
